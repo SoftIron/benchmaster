0cafd1b1d2f8ad5f8d2c11e1bf8b31cd7f77cd69
Harry Richardson <harry@softiron.com>
2020-04-28 08:14:04 -0400

    Made cosbench path absolute

--- benchmaster/cosbench.py
+++ benchmaster/cosbench.py
@@ -12,7 +12,7 @@ import time
 
 from datetime import datetime
 
-_cosbench_dir = 'cosbench_0.4.2.c4'
+_cosbench_dir = '/root/cosbench_0.4.2.c4'
 
 
 def _header(test_name, time, access_key, secret_key, gateway, port):
